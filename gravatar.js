98e907d48d5ba21c1940291b26b007aaaae91c41
Dave Reid <dave@davereid.net>
2009-07-29 16:27:23 +0000

    by Dave Reid: Merged with 6.x and updated for 7.x.

---
+++ gravatar.js
@@ -0,0 +1,14 @@
+// $Id$
+
+Drupal.behaviors.gravatarPreview = function (context) {
+  $('input[name=gravatar_default]:not(.gravatarPreview-processed)', context).addClass('gravatarPreview-processed').click(function () {
+    var selected_image = $('img#gravatar-imagepreview-' + this.value);
+    $('img#gravatar-imagepreview').attr('src', selected_image.attr('src'));
+  });
+  $(document).ready(function () {
+    var selected_index = $('input[name=gravatar_default][checked]').val();
+    var selected_image = $('img#gravatar-imagepreview-' + selected_index);
+    $('img#gravatar-imagepreview').attr('src', selected_image.attr('src'));
+    //$('.js-show').show();
+  });
+};
