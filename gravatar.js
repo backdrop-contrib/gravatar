1cdb724ece9efea8dc05543564ebe8816df20dbd
Dave Reid <dave@davereid.net>
2010-05-29 20:25:55 +0000

    by Dave Reid: Fixed broken JavaScript and links to admin pages.

--- gravatar.js
+++ gravatar.js
@@ -1,14 +1,23 @@
 // $Id$
 
-Drupal.behaviors.gravatarPreview = function (context) {
-  $('input[name=gravatar_default]:not(.gravatarPreview-processed)', context).addClass('gravatarPreview-processed').click(function () {
-    var selected_image = $('img#gravatar-imagepreview-' + this.value);
-    $('img#gravatar-imagepreview').attr('src', selected_image.attr('src'));
-  });
-  $(document).ready(function () {
-    var selected_index = $('input[name=gravatar_default][checked]').val();
-    var selected_image = $('img#gravatar-imagepreview-' + selected_index);
-    $('img#gravatar-imagepreview').attr('src', selected_image.attr('src'));
-    //$('.js-show').show();
-  });
+(function ($) {
+
+Drupal.behaviors.gravatarPreview = {
+  attach: function (context) {
+    $('input[name=gravatar_default]', context).once('gravatarPreview', function () {
+      $(this).bind('change', function() {
+        var selected_image = $('img#gravatar-imagepreview-' + $(this).val());
+        $('img#gravatar-imagepreview').attr('src', selected_image.attr('src'));
+      });
+    });
+
+    $(document).ready(function () {
+      var selected_index = $('input[name=gravatar_default][checked]').val();
+      var selected_image = $('img#gravatar-imagepreview-' + selected_index);
+      $('img#gravatar-imagepreview').attr('src', selected_image.attr('src'));
+      $('.js-show').show();
+    });
+  }
 };
+
+})(jQuery);
